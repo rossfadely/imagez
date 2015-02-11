911ec9df72c686f6041006869ca5470f764ce3fa
Ross Fadely <rossfadely@gmail.com>
2015-02-11 21:40:02 +0000

    Changing data munging workflow

--- code/data.py
+++ code/data.py
@@ -1,3 +1,6 @@
+from matplotlib import use; use('Agg')
+from skimage.transform import downscale_local_mean, rotate
+
 import os
 import glob
 import time
@@ -5,8 +8,8 @@ import warnings
 import multiprocessing
 import numpy as np
 import pyfits as pf
+import matplotlib.pyplot as pl
 
-from skimage.transform import downscale_local_mean
 
 def query_sdss(save_dir, run, rerun, camcol, field, context='dr12'):
     """
@@ -153,87 +156,7 @@ def get_orig_patch(data, patch_size, row, col):
     assert patch_size % 2 == 1, 'Patch size should be odd'
     dlt = (patch_size - 1) / 2
     patch = data[row - dlt - 1:row + dlt, col - dlt - 1:col + dlt]
-    return patch.ravel()
-
-def make_orig_patches(photfile, specfile, img_dir, patch_dir, patch_size=25,
-                      tol=1.e-3, start=0, end=None, context='dr12'):
-    """
-    Cut out patches in the images and save to disk.
-    """
-    os.chdir(img_dir)
-    f = pf.open(photfile)
-    data = f[1].data
-    f.close()
-
-    f = pf.open(specfile)
-    spec = f[1].data
-    f.close()
-
-    if end == None:
-        end = len(data)
-
-    for i in range(start, end):
-        if i % 5 == 0:
-            print 'Processing patch', i
-
-        # match to spectral info, reject if necessary
-        ind = np.where(data['specobjid'][i] == spec['specobjid'])[0]
-        if len(ind) > 1:
-            ind = ind[0] # some repeat images of same object!!
-        specinfo = spec[ind]
-
-        run = str(data['run'][i])
-        field = str(data['field'][i])
-        camcol = str(data['camcol'][i])
-
-        nz = 6 - len(run)
-        filled_run = '0' * nz + run
-        nz = 4 - len(field)
-        filled_field = '0' * nz + field
-
-        filts = 'ugriz'
-        if context == 'dr7':
-            frames = ['./%s/fpC-%s-%s-%s.fit.gz' % (run, filled_run,
-                                                    f + camcol,
-                                                    filled_field)
-                      for f in filts]
-        else:
-            frames = ['./%s/frame-%s-%s-%s-%s.fits.bz2' % (run, f, filled_run,
-                                                           camcol,
-                                                           filled_field)
-                      for f in 'ugriz']
-
-        patch = np.zeros((patch_size ** 2., 5))
-        patch_file = patch_dir + 'orig_%s.fits' % str(data['specobjid'][i])
-        if os.path.exists(patch_file):
-            continue
-
-        for j in range(5):
-            if not os.path.exists(frames[j]):
-                print glob.glob('ls -l')
-                print os.getcwd()
-                print i, j, data[i]
-                print frames[j]
-                assert False, 'Image frame has not been downloaded.'
-
-            # unpack data and read image
-            img_file = frames[j]
-            if context == 'dr7':
-                os.system('gunzip %s' % frames[j])
-                f = pf.open(frames[j][:-3])
-                os.system('gzip %s' % frames[j][:-3])
-            else:
-                os.system('bzip2 -d %s' % frames[j])
-                f = pf.open(frames[j][:-4])
-                os.system('bzip2 %s' % frames[j][:-4])
-            img = f[0].data
-            f.close()
-            patch[:, j] = get_orig_patch(img, patch_size,
-                                         data['rowc_' + filts[j]][i],
-                                         data['colc_' + filts[j]][i])
-
-        hdu = pf.PrimaryHDU(patch)
-        hdu.writeto(patch_file)
+    return patch
 
 def downsample(patch, factor, save_path=None):
     """
@@ -245,130 +168,134 @@ def downsample(patch, factor, save_path=None):
         hdu.writeto(save_path)
     return new
 
-def gen_final_files(data_dir, save_base, photfile, specfile, factor=4,
-                    orig_size=25, divide_out='avg', include=['psf']):
+def make_centered_rotated_patches(photfile, patch_dir, ind_file, PA_final=45.,
+                                  pshape=(25, 25), start=0, end=None,
+                                  do_shift=True, do_rotation=True,
+                                  floor=1.e-5):
     """
-    Take the original patches, apply any transformations, and save to 
-    single file.  Also save redshifts.
+    Patchify original image, use skimage to shift to common center, rotate to
+    common angle, and save patch.
     """
-    target_path = save_base + '_z.txt'
-    feature_path = save_base + '_features.fits'
-    patch_paths = glob.glob(data_dir + '*fits')
-    
-    # Need photfile for extinction, etc.
+    assert end != -1, 'Use actual end, not -1'
+    f = pf.open(photfile)
+    data = f[1].data
+    f.close()
+
+    # only gri get to vote on rotation angle
+    angles = data['fracdev_g'] * data['devphi_g']
+    angles += (1. - data['fracdev_g']) * data['expphi_g']
+    for f in 'ri':
+        angles += data['fracdev_' + f] * data['devphi_' + f]
+        angles += (1. - data['fracdev_' + f]) * data['expphi_' + f]
+    angles /= 3.
+
+    # read spec z file with indicies in first column
+    info = np.loadtxt(ind_file)
+    if end is None:
+        end = len(info[:, 0])
+    inds = info[start:end, 0].astype(np.int)
+
+    for i in inds:
+        patch_file = patch_dir + 'orig_%s.fits' % str(data['specobjid'][i])
+        if os.path.exists(patch_file):
+            continue
+
+        run = str(data['run'][i])
+        field = str(data['field'][i])
+        camcol = str(data['camcol'][i])
+
+        nz = 6 - len(run)
+        filled_run = '0' * nz + run
+        nz = 4 - len(field)
+        filled_field = '0' * nz + field
+
+        filts = 'ugriz'
+        frames = ['./%s/frame-%s-%s-%s-%s.fits.bz2' % (run, f, filled_run,
+                                                       camcol, filled_field)
+                  for f in 'ugriz']
+
+        out_patch_data = np.zeros((patch_size ** 2., 5))
+        for j in range(5):
+            # check that image exists
+            if not os.path.exists(frames[j]):
+                print frames[j]
+                assert False, 'Image frame has not been downloaded.'
+
+            # unpack data and read image
+            img_file = frames[j]
+            os.system('bzip2 -d %s' % frames[j])
+            f = pf.open(frames[j][:-4])
+            os.system('bzip2 %s' % frames[j][:-4])
+            img = f[0].data
+            f.close()
+
+            # floor the row and col centers
+            flrr = np.floor(data['rowc_' + filts[j]][i])
+            flrc = np.floor(data['colc_' + filts[j]][i])
+
+            # get patch centered on the floored centers
+            patch = get_orig_patch(img, patch_size, flrr, flrc)
+            if floor is not None:
+                patch = np.maximum(floor, patch)
+
+            if do_shift | do_rotation:
+                pmn, pmx = patch.min(), patch.max()
+                rng = pmx - pmn
+                patch = (patch - pmn) / rng
+                shift, rotation = None
+                if do_shift:
+                    # find subpixel shift
+                    dltr = data['rowc_' + filts[j]][i] - flrr - 0.5
+                    dltc = data['colc_' + filts[j]][i] - flrc - 0.5
+                    shift = -1. * np.array([dltr, dltc])
+                if do_rotation:
+                    rotation = np.deg2rad(45. - angles[i])
+                tform = AffineTransform(rotation=rotation, translation=shift)
+                patch = warp(patch, tform)
+                patch = patch * rng + pmn
+
+            assert False, 'Check the above is cool.'
+
+def generate_zspec_file(photfile, zspec_file):
+    """
+    Run through the photo data and generate a list of spec z's and indicies on
+    the file.  Necessary since same obj has multiple spectra in a bunch of
+    cases.
+    """
     f = pf.open(photfile)
     photdata = f[1].data
     f.close()
-    photdata = photdata[np.argsort(photdata['specobjid'])]
+    N = len(photdata)
 
-    # Need specinfo
-    f = pf.open(specfile)
-    specdata = f[1].data
-    f.close()
-    specdata = specdata[np.argsort(specdata['specobjid'])]
+    i = 0
+    inds = np.zeros(N, dtype=np.int) - 1
+    speczs = np.zeros(N)
+    speczerrs = np.zeros(N)
+    while i < N:
+        inds[i] = i
+        speczs[i] = photdata['specz'][i]
+        speczerrs[i] = photdata['specz_err'][i]
+        if i != (N - 1):
+            if photdata['objid'][i] == photdata['objid'][i + 1]:
+                if photdata['objid'][i] == photdata['objid'][i + 2]:
+                    print i
+                    assert 0
+                w = 1. / photdata['specz_err'][i:i + 2] ** 2.
+                norm = w.sum()
+                speczs[i] = photdata['specz'][i] * w[0]
+                speczs[i] += photdata['specz'][i + 1] * w[1]
+                speczs[i] /= norm
+                speczerrs[i] = 1. / np.sqrt(norm)
+                i += 1
+        i += 1
 
-    # tranform one patch to get shape
-    f = pf.open(patch_paths[0])
-    d = f[0].data[:, 0].reshape(orig_size, orig_size)
-    f.close()
-    d = downsample(d, factor)
-    patch_size = d.size
-
-    Ndim = patch_size * 5
-    if 'psf' in include:
-        Ndim += 5
-    Nsamples = len(patch_paths)
-    patch_data = np.zeros((Nsamples, Ndim))
-
-    targ_line = ''
-    for i in range(Nsamples):
-        if i % 100 == 0:
-            print 'working on', i
-        
-        f = pf.open(patch_paths[i])
-        d = f[0].data
-        f.close()
-
-        specobjid = np.int(patch_paths[i].split('_')[-1][:-5])
-        ind = np.searchsorted(photdata['specobjid'], specobjid)
-
-        includes = -1
-        for j, f in enumerate('rugiz'):
-            patch = d[:, j].reshape(orig_size, orig_size)
-            patch *= photdata[ind]['extinction_' + f]
-            patch = downsample(patch, factor).ravel()
-            if j == 0: # always keep original r patch
-                norm_patch = patch 
-            elif divide_out == 'patch':
-                patch /= norm_patch
-            elif divide_out == 'avg':
-                patch /= np.maximum(np.mean(norm_patch), 1.)
-            patch_data[i, patch_size * j: patch_size * (j + 1)] = patch
-
-            if 'psf' in include:
-                name = 'mrrccpsf_' + f
-                patch_data[i, includes] = np.sqrt(photdata[ind][name])
-                includes -= 1
-
-        ind = np.searchsorted(specdata['specobjid'], specobjid)
-        targ_line += str(specdata['specobjid'][ind]) + ' '
-        targ_line += str(specdata['z'][ind]) + ' '
-        targ_line += str(specdata['zerr'][ind]) + '\n'
-
-    f = open(target_path, 'w')
-    f.write('# specobjid, z, zerr\n')
-    f.write(targ_line)
-    f.close()
-    hdu = pf.PrimaryHDU(patch_data)
-    hdu.writeto(feature_path, clobber=True)
-
-def match_photozs_to_orig(patch_dir, photozfile, photfile, outpath):
-    """
-    Match photoz values to objects that have patches.
-    """
-    patch_paths = glob.glob(patch_dir + '*fits')
-
-    f = pf.open(photfile)
-    photdata = f[1].data
-    f.close()
-    photdata = photdata[np.argsort(photdata['specobjid'])]
-
-    f = pf.open(photozfile)
-    photozdata = f[1].data
-    f.close()
-    photozdata = photozdata[np.argsort(photozdata['objid'])]
-
-    Nsamples = len(patch_paths)
-    out_line = ''
-    for i in range(Nsamples):
-        if i % 100 == 0:
-            print 'working on', i
-
-        f = pf.open(patch_paths[i])
-        d = f[0].data
-        f.close()
-
-        specobjid = np.int(patch_paths[i].split('_')[-1][:-5])
-        ind = np.searchsorted(photdata['specobjid'], specobjid)
-        ind = np.searchsorted(photozdata['objid'], photdata[ind]['objid'])
-        assert (ind >= 0)
-        d = photozdata[ind]
-        out_line += ' '.join([str(specobjid), str(d['objid']), str(d['z']),
-                              str(d['zerr'])]) + '\n'
-
-    f = open(outpath, 'w')
-    f.write('# specobjid, objid, z, zerr\n')
-    f.write(out_line)
-    f.close()
+    ind = np.where(inds >= 0)[0]
+    print ind.shape
 
 if __name__ == '__main__':
     # Run the script
     img_dir = os.environ['IMGZDATA']
     photfile = img_dir + 'dr12_main_50k_rfadely.fit'
-    specfile = photfile
-    photozfile = photfile
-    #specfile = img_dir + 'dr7spec2nddr724k_rfadely.fit'
-    #photozfile = img_dir + 'dr7photozdr724k_rfadely.fit'
     patch_dir = img_dir + 'patches/dr12/orig/'
     data_dir = img_dir + 'patches/dr12/'
     img_dir += 'dr12/'
@@ -376,21 +303,10 @@ if __name__ == '__main__':
     if False:
         get_images(photfile, img_dir, start=32500, end=35000)
 
-    if False:
-        make_img_list(photfile, img_dir, img_dir + 'dr724k_imgs.txt')
-
     if True:
-        warnings.filterwarnings("ignore")
-        s, e = 40000, 50000
-        make_orig_patches(photfile, specfile, img_dir, patch_dir, start=s,
-                          end=e)
+        zspec_file = img_dir + 'dr12_main_50k_z-ind.txt'
+        generate_zspec_file(photfile, zspec_file)
 
     if False:
-        gen_final_files(patch_dir, data_dir + 'dr7_fact4_avg_withpsf',
-                        photfile, specfile)
-
-    if False:
-        outpath = data_dir + 'dr7_fact4_avg_withpsf_photoz.txt'
-        match_photozs_to_orig(patch_dir, photozfile, photfile, outpath)
-
+        make_rotated_patches(photfile, patch_dir)
 
